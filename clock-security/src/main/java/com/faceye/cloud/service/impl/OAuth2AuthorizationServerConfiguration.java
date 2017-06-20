dad6a9405a2f28ff37bbe670136d2f6b2afa6808
haipenge@gmail.com <haipenge@gmail.com>
2017-06-21 07:40:24 +0800

    update spring cloud security

--- clock-security/src/main/java/com/faceye/cloud/service/impl/OAuth2AuthorizationServerConfiguration.java
+++ clock-security/src/main/java/com/faceye/cloud/service/impl/OAuth2AuthorizationServerConfiguration.java
@@ -10,8 +10,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.security.oauth2.authserver.AuthorizationServerProperties;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -20,14 +18,17 @@ import org.springframework.security.access.vote.AuthenticatedVoter;
 import org.springframework.security.access.vote.RoleVoter;
 import org.springframework.security.access.vote.UnanimousBased;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.approval.ApprovalStore;
 import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
 import org.springframework.security.oauth2.provider.approval.TokenStoreUserApprovalHandler;
@@ -38,15 +39,18 @@ import org.springframework.security.oauth2.provider.client.JdbcClientDetailsServ
 import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
 import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
 import org.springframework.security.oauth2.provider.error.OAuth2AuthenticationEntryPoint;
+import org.springframework.security.oauth2.provider.request.DefaultOAuth2RequestFactory;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import org.springframework.security.web.access.expression.WebExpressionVoter;
 
-import com.faceye.component.security.web.service.ClientDetailsService;
 import com.faceye.component.security.web.service.UserService;
 
 @Configuration
 @EnableAuthorizationServer
 @Order(2)
-public class OAuth2AuthorizationServerConfiguration extends AuthorizationServerProperties {
+public class OAuth2AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
 
 	private Logger logger = LoggerFactory.getLogger(OAuth2AuthorizationServerConfiguration.class);
 	@Autowired
@@ -67,7 +71,7 @@ public class OAuth2AuthorizationServerConfiguration extends AuthorizationServerP
 	
 //	private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-	@Override
+//	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
 		security.checkTokenAccess("permitAll()");
 		security.allowFormAuthenticationForClients();
@@ -80,6 +84,7 @@ public class OAuth2AuthorizationServerConfiguration extends AuthorizationServerP
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.withClientDetails(clientDetails());
+		
 	}
 
 	@Override
@@ -214,11 +219,11 @@ public class OAuth2AuthorizationServerConfiguration extends AuthorizationServerP
 	
 	@Configuration
 	@EnableResourceServer
-	public static class ResourceServerConfiguration extends ResourceServerProperties {
+	public static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
 		@Autowired
 		private DefaultTokenServices tokenServices = null;
 
-		@Override
+//		@Override
 		public void configure(ResourceServerSecurityConfigurer resources) {
 			resources.resourceId(API_RESOURCE_ID);
 			resources.tokenServices(tokenServices);
